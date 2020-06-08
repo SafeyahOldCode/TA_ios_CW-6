8b82546213ca60b362cfea7beb78a22c18bf3b79
Hasan Alsaffar <h.i.i.s@hotmail.com>
2020-06-08 21:56:08 +0300

    Initial Commit

---
+++ CW-6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CW-6
+//
+//  Created by Hasan Alsaffar on 6/8/20.
+//  Copyright © 2020 Hasan Alsaffar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
